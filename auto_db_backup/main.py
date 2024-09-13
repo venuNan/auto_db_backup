11b3a1ca77aad31455a99cb4dd7e8e95839f5cce
venuNan <venureddy3808@gmail.com>
2024-09-13 22:51:02 +0530

    changed the functions format in main.py file and added a common function for same options which is used by different types of commands and added some neccasr modules to the toml file

--- auto_db_backup/main.py
+++ auto_db_backup/main.py
@@ -10,79 +10,117 @@ def cli(version):
     if version:
         click.echo(f"Auto_db_backup   {APP_VERSION}")
 
+def common_options(func):
+    func = click.option("--storage_option", "-so", required=True, type=click.Choice(["local", "cloud"]), help="Where to store the backup, in the cloud or local system")(func)
+    func = click.option("--compress", "-c", is_flag=True, help="Store the files in a compressed format like gzip or tar")(func)
+    func = click.option("--logging", "-log", is_flag=True, help="Log the operations performed on the database and the timing of the backup")(func)
+    func = click.option("--auto_backup", "-ab", is_flag=True, default=False, help="Automate the backup process")(func)
+    func = click.option('--backup_dir', default=f'{os.getcwd()}', hidden=True)(func)
+    func = click.option("--passwd", "-pw", required=True, help="Password of the database")(func)
+    func = click.option("--database_name", "-db_name", required=True, help="Name of the database you want to backup")(func)
+    func = click.option("--username", "-u", required=True, help="Username of the database")(func)
+    func = click.option("--port", "-p", required=True, help="Port number of the database")(func)
+    func = click.option("--host", "-h", required=True, help="The IP address of the database")(func)
+    return func
+
+def tables_option(func):
+    func = click.option("--tables", "-t", multiple=True, required=False, help="Specify tables to back up. Use multiple '-t table_name' for multiple tables.")(func)
+    return func
+
+def auto_backup():
+       pass
+
 @click.command()
-@click.option("--database","-db",
-              type=click.Choice(["mysql", "postgresql","sqlite3","sqlserver","mariadb","oracledb","sybase","teradata"]),
-              required=True ,
-              help="Type of databse EX:- MYSQL, postgresql, SQLServer, SQlite3, tec.")
-@click.option("--host", "-h", required=True, help="The ip addrees of the database")
-@click.option("--port", "-p", required=True, help="Port number of the databse")
-@click.option("--username", "-u", required=True, help="Username of the database")
-@click.option("--database_name", "-db_name", required=True, help="Name of the database you want to backup")
-@click.option("--passwd", "-pw", required=True, help="Password of the database")
-@click.option("--tables", "-t", multiple=True, required=False, help="If you want to backup certain tables in the databse.\n If you want to add multiple tables enter like '-t table_name1 -t table_name2'")
-@click.option("--compress", "-c", is_flag=True, help="To store the files in a compressed format like gzip and tar files")
-@click.option("--storage_option", "-so", required=True, type=click.Choice(["local","cloud"]), help="Where to store the backup in the cloud or local system")
-@click.option("--logging", "-log", is_flag=True, help="To log the operation perfomed on the databse and the timing of the backup")
-@click.option("--auto_backup", "-ab", is_flag=True, default="no",help="To the automate the backup process")
-@click.option('--backup_dir', default=f'{os.getcwd()}', hidden=True)
-def backup(database, host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
-
-    if database=="mysql":
-        db_backup.backup_mysql(host, username, passwd, database_name, tables, compress, storage_option, logging, auto_backup, backup_dir, port_no=port )
-
-    if database == "mysql":
+@common_options
+@tables_option
+def backup_mysql(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_mysql(
             host, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir, port_no=port
         )
-
-    elif database == "postgresql":
+        if auto_backup:
+            print("auto backup intialised")
+@click.command()
+@common_options
+@tables_option
+def backup_postgresql(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_postgresql(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    elif database == "sqlite3":
+@click.command()
+@common_options
+@tables_option
+def backup_sqlite3(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_sqlite3(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    elif database == "sqlserver":
+@click.command()
+@common_options
+@tables_option
+def backup_sqlserver(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_sqlserver(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    elif database == "mariadb":
+@click.command()
+@common_options
+@tables_option
+def backup_mariadb(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_mariadb(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    elif database == "oracledb":
+@click.command()
+@common_options
+@tables_option
+def backup_oracledb(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_oracledb(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    elif database == "sybase":
+@click.command()
+@common_options
+@tables_option
+def backup_sybase(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_sybase(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    elif database == "teradata":
+@click.command()
+@common_options
+@tables_option
+def backup_teradata(host, port, username, passwd,database_name, tables, compress, storage_option, logging, auto_backup, backup_dir):
         db_backup.backup_teradata(
             host, port, username, passwd, database_name, tables, compress, storage_option, 
             logging, auto_backup, backup_dir
         )
 
-    else:
-        click.echo(f"Unsupported database: {database}")
+@click.command()
+@common_options
+@click.option("--collection_name","-col", multiple=True, required=False, help="Specify collections to back up. Use multiple '-col collection_name' for multiple collections.")
+def backup_mongodb(host, port, username, passwd,database_name, collection_name, compress, storage_option, logging, auto_backup, backup_dir):
+        db_backup.backup_mongodb(
+            host, port, username, passwd, database_name, collection_name, compress, storage_option, 
+            logging, auto_backup, backup_dir
+        )
 
+cli.add_command(backup_mysql)
+cli.add_command(backup_postgresql)
+cli.add_command(backup_sqlite3)
+cli.add_command(backup_sqlserver)
+cli.add_command(backup_sybase)
+cli.add_command(backup_oracledb)
+cli.add_command(backup_mariadb)
+cli.add_command(backup_mongodb)
+cli.add_command(backup_teradata)
 
-cli.add_command(backup)
 if __name__ == "__main__":
     cli()
